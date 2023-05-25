56cb81d42e54abd1e958e565662f901955a8cfa9
HeeeWoon <heewoon9210@naver.com>
2023-05-25 13:37:20 +0900

    Add files via upload

---
+++ JavaStudy/ex04반복/src/for문/Ex03for문예제.java
@@ -0,0 +1,14 @@
+package for문;
+
+public class Ex03for문예제 {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i < 10; i++) {
+			int a = 2 * i;
+			System.out.println("2 * " + i + "=" + a);
+		}
+
+	}
+
+}

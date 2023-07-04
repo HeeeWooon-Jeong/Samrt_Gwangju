6940155ce0428571f31a750621a1379c59dddc1c
HeeeWoon <heewoon9210@naver.com>
2023-07-04 19:32:36 +0900

    Add files via upload

---
+++ PythonLib/ex00_module_test.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# 1. 더하기 함수
+# 매개변수 0 , return 0
+
+def add(num1, num2):
+    return num1+num2
+
+
+# In[ ]:
+
+
+# 2. 빼기 함수
+def sub(num1, num2):
+    return num1 - num2
+
+# 3. 곱하기 함수
+
+
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
